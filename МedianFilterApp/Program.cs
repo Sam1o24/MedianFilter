f62805663c7f2d8e374d650b5c616b2c695f1c90
SAM <jim1024@yandex.ru>
2014-12-06 02:13:52 +0500

    commit

--- МedianFilterApp/Program.cs
+++ МedianFilterApp/Program.cs
@@ -6,6 +6,7 @@ using System.Runtime.InteropServices;
 using System.Threading.Tasks;
 using System.Drawing;
 using System.Drawing.Imaging;
+using System.Threading;
 
 namespace МedianFilterApp
 {
@@ -14,29 +15,53 @@ namespace МedianFilterApp
         static void Main(string[] args)
         {
            
-            //Bitmap bmp = new Bitmap("test.bmp");
+            //Bitmap bmp = new Bitmap("test.bmp"); //входной файл
             Bitmap bmp = new Bitmap(args[0]);
 
-            int PCount = Environment.ProcessorCount;
+            int PCount = Environment.ProcessorCount; //количество "процессоров"
 
             BitmapData bd = bmp.LockBits(new Rectangle(0, 0, bmp.Width, bmp.Height),ImageLockMode.ReadWrite,bmp.PixelFormat);
 
-            int part = Convert.ToInt32(bd.Stride / PCount * bd.Height);
+            int part = Convert.ToInt32(bd.Stride / PCount * bd.Height); //размер одной части
 
-            byte[][] buffer = new byte[PCount][];
-            for (int i = 0; i <  PCount; i++)
+            int width = Convert.ToInt32(bd.Width / PCount);     //ширина одной части
+            int height = bd.Height;                             //высота одной части
+
+            Part[] m_part = new Part[PCount];
+
+            //заполнение частей
+            for (int i = 0; i < PCount; i++)
             {
-                buffer[i] = new byte[part];
-                Marshal.Copy(bd.Scan0 + i * buffer[i].Length, buffer[i], 0, buffer[i].Length);
+                m_part[i] = new Part(i, width, height);
+                m_part[i].Buff = new byte[part];
+                Marshal.Copy(bd.Scan0 + i * m_part[i].Buff.Length, m_part[i].Buff, 0, m_part[i].Buff.Length);
             }
 
-            int with = Convert.ToInt32(bd.Width / PCount);
+            //Parallel.ForEach<byte[]>(buffer, item => ProcessingPart(item, with, bd.Height));
 
-            Parallel.ForEach<byte[]>(buffer, item => ProcessingPart(item, with, bd.Height));
+            Thread[] m_th = new Thread[PCount];
+
+            //создание потоков
+            for (int i = 0; i < PCount; i++)
+            {
+                m_th[i] = new Thread(m_part[i].ProcessingPart);
+            }
+
+            //запуск потоков
+            for (int i = 0; i < PCount; i++)
+            {
+                m_th[i].Start();
+            }
+
+            //ожидание результатов
+            for (int i = 0; i < PCount; i++)
+            {
+                //m_th[i].Join();
+            }
 
             for (int i = 0; i < PCount; i++)
             {
-                Marshal.Copy(buffer[i], 0, bd.Scan0 + i * buffer[i].Length, buffer[i].Length);
+                Marshal.Copy(m_part[i].Buff, 0, bd.Scan0 + i * m_part[i].Buff.Length, m_part[i].Buff.Length);   //сбор частей в единую картинку
             }
                 bmp.UnlockBits(bd);
                 bmp.Save(args[1]);
@@ -44,47 +69,9 @@ namespace МedianFilterApp
                 bmp.Dispose();
         }
 
-        static byte[] ProcessingPart(byte[] buffer, int width, int height)
-        {
-            for (int k = 0; k < 3; k++)
-            {
-                ChangePixColor(ref buffer, width, height,k);
-            }
 
-            return buffer;
-        }
 
-        static void ChangePixColor(ref byte[] buffer, int width, int height, int k)
-        {
-            int Stride = width * 3;
 
-            byte[] temp = new byte[9];
-            byte[] buffer_copy = new byte[buffer.Length];
-            Array.Copy(buffer, buffer_copy, buffer.Length);
-
-            for (int i = 1; i < width - 1; i++)
-            {
-                for (int j = 1; j < height - 1; j++)
-                {
-                    temp[0] = buffer[Stride * (j - 1) + 3 * (i - 1) + k];
-                    temp[1] = buffer[Stride * (j - 1) + 3 * i + k];
-                    temp[2] = buffer[Stride * (j - 1) + 3 * (i + 1) + k];
-
-                    temp[3] = buffer[Stride * j + 3 * (i - 1) + k];
-                    temp[4] = buffer[Stride * j + 3 * i + k];
-                    temp[5] = buffer[Stride * j + 3 * (i + 1) + k];
-
-                    temp[6] = buffer[Stride * (j + 1) + 3 * (i - 1) + k];
-                    temp[7] = buffer[Stride * (j + 1) + 3 * i + k];
-                    temp[8] = buffer[Stride * (j + 1) + 3 * (i + 1) + k];
-
-                    Array.Sort(temp);
-
-                    buffer_copy[Stride * j + 3 * i + k] = temp[4];
-                }
-            }
-            Array.Copy(buffer_copy, buffer, buffer_copy.Length);
-        }
         
     }
 }
